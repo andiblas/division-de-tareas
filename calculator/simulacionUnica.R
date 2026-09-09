fb09a39300075f7b0568ff8f80d72eecce7cafeb
andiblas <andiblas@gmail.com>
2026-09-09 17:46:28 -0300

    add simulacion run files

---
+++ calculator/simulacionUnica.R
@@ -0,0 +1,27 @@
+source("repartoTareas.R")
+
+# nRep = 1000
+# maximos = vector(,nRep)
+# n_tareas = 20
+# n_agentes = 4
+# alfaVec = rep(0.482087, n_tareas)
+# for (i in 1:nRep) {
+#   X1 = rdirichlet(1, alfaVec)
+#   maximos[i] = max(X1)
+# }
+# mean(maximos)
+
+LoteInicio = 1
+LoteFin = 2
+CantidadDeInstanciasPorCotizacion = 7
+CantidadDeSegundosPorAlgoritmo = 3
+Lambda = 100
+Agentes = 2
+Tareas = 6
+simulacionTareas(LoteInicio, 
+                LoteFin, 
+                CantidadDeInstanciasPorCotizacion,
+                CantidadDeSegundosPorAlgoritmo, 
+                Lambda,
+                Agentes,
+                Tareas)
